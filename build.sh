9812c7aae75d74145474e644036b053ecf64776d
Alex Bigelow <alex.r.bigelow@gmail.com>
2019-01-25 12:41:54 -0700

    Many fixes, attempting to set up environment for running tests

--- build.sh
+++ build.sh
@@ -5,58 +5,70 @@ set -e
 PROCS=$(lscpu | grep CPU.s.: | head -1 | cut -d: -f2)
 USE_PROCS=$(($PROCS/2))
 
-NOW=$(date +"%Y-%m-%dT%H:%M:%S")
-BUILD_DATE=${1:-$NOW}
+BUILD_DATE=date --date="${1:-`date`}" +"%s"
+HUMAN_BUILD_DATE=`date --date=@$BUILD_DATE`
 BUILD_DIR=${BUILD_DIR:-"$HOME/build-$BUILD_DATE"}
 BUILD_FILE="$BUILD_DIR/setup.sh"
-echo "Writing build file: $BUILD_FILE"
 if [ -d $BUILD_DIR ]
 then
   rm -rf $BUILD_DIR
 fi
 mkdir $BUILD_DIR
 touch $BUILD_FILE
+touch $BUILD_DIR/log.txt
 cd `dirname $0`
 cp run.sh $BUILD_DIR
 
+echo "Starting retroactive build for $BUILD_DATE (unix timestamp for $HUMAN_BUILD_DATE)"
+echo "Starting retroactive build for $BUILD_DATE (unix timestamp for $HUMAN_BUILD_DATE)" >> $BUILD_DIR/log.txt
+
 echo "#!/bin/bash" >> $BUILD_FILE
-echo "BUILD_DATE=\"$BUILD_DATE\"" >> $BUILD_FILE
+echo "# Retroactive settings for building and/or running $BUILD_DATE (unix timestamp for $HUMAN_BUILD_DATE)" >> $BUILD_FILE
 
 # Load modules
 CLANG_VERSION=${CLANG_VERSION:-6.0.1}
-echo "CLANG_VERSION=\"$CLANG_VERSION\"" >> $BUILD_FILE
+echo "export CLANG_VERSION=\"$CLANG_VERSION\"" >> $BUILD_FILE
+echo "module load clang/$CLANG_VERSION" >> $BUILD_FILE
 module load clang/$CLANG_VERSION
 
 CMAKE_VERSION=${CMAKE_VERSION:-3.9.0}
-echo "CMAKE_VERSION=\"$CMAKE_VERSION\"" >> $BUILD_FILE
+echo "export CMAKE_VERSION=\"$CMAKE_VERSION\"" >> $BUILD_FILE
+echo "module load cmake/$CMAKE_VERSION" >> $BUILD_FILE
 module load cmake/$CMAKE_VERSION
 
 GPERFTOOLS_VERSION=${GPERFTOOLS_VERSION:-2.7}
-echo "GPERFTOOLS_VERSION=\"$GPERFTOOLS_VERSION\"" >> $BUILD_FILE
+echo "export GPERFTOOLS_VERSION=\"$GPERFTOOLS_VERSION\"" >> $BUILD_FILE
+echo "module load gperftools/$GPERFTOOLS_VERSION" >> $BUILD_FILE
 module load gperftools/$GPERFTOOLS_VERSION
 
 BOOST_VERSION=${BOOST_VERSION:-1.68.0-clang6.0.1-debug}
-echo "BOOST_VERSION=\"$BOOST_VERSION\"" >> $BUILD_FILE
+echo "export BOOST_VERSION=\"$BOOST_VERSION\"" >> $BUILD_FILE
+echo "module load boost/$BOOST_VERSION" >> $BUILD_FILE
 module load boost/$BOOST_VERSION
 
 HWLOC_VERSION=${HWLOC_VERSION:-2.0.0}
-echo "HWLOC_VERSION=\"$HWLOC_VERSION\"" >> $BUILD_FILE
+echo "export HWLOC_VERSION=\"$HWLOC_VERSION\"" >> $BUILD_FILE
+echo "module load hwloc/$HWLOC_VERSION" >> $BUILD_FILE
 module load hwloc/$HWLOC_VERSION
 
 PAPI_VERSION=${PAPI_VERSION:-5.6.0}
-echo "PAPI_VERSION=\"$PAPI_VERSION\"" >> $BUILD_FILE
+echo "export PAPI_VERSION=\"$PAPI_VERSION\"" >> $BUILD_FILE
+echo "module load papi/$PAPI_VERSION" >> $BUILD_FILE
 module load papi/$PAPI_VERSION
 
 BLAZE_VERSION=${BLAZE_VERSION:-3.4}
-echo "BLAZE_VERSION=\"$BLAZE_VERSION\"" >> $BUILD_FILE
+echo "export BLAZE_VERSION=\"$BLAZE_VERSION\"" >> $BUILD_FILE
+echo "module load blaze/$BLAZE_VERSION" >> $BUILD_FILE
 module load blaze/$BLAZE_VERSION
 
 PYBIND11_VERSION=${PYBIND11_VERSION:-2.2.4}
-echo "PYBIND11_VERSION=\"$PYBIND11_VERSION\"" >> $BUILD_FILE
+echo "export PYBIND11_VERSION=\"$PYBIND11_VERSION\"" >> $BUILD_FILE
+echo "module load pybind11/$PYBIND11_VERSION" >> $BUILD_FILE
 module load pybind11/$PYBIND11_VERSION
 
 PYTHON_VERSION=${PYTHON_VERSION:-3.6.3s}
-echo "PYTHON_VERSION=\"$PYTHON_VERSION\"" >> $BUILD_FILE
+echo "export PYTHON_VERSION=\"$PYTHON_VERSION\"" >> $BUILD_FILE
+echo "module load python/$PYTHON_VERSION" >> $BUILD_FILE
 module load python/$PYTHON_VERSION
 
 INSTALL_DIR=${INSTALL_DIR:-$HOME/install}
@@ -68,7 +80,7 @@ cd $INSTALL_DIR
 
 # Install OTF2 if needed
 OTF2_VERSION=${OTF2_VERSION:-2.1.1}
-echo "OTF2_VERSION=\"$OTF2_VERSION\"" >> $BUILD_FILE
+echo "export OTF2_VERSION=\"$OTF2_VERSION\"" >> $BUILD_FILE
 if [ ! -d $INSTALL_DIR/otf2 ]
 then
   mkdir $INSTALL_DIR/otf2
@@ -76,6 +88,7 @@ fi
 if [ ! -d $INSTALL_DIR/otf2/$OTF2_VERSION ]
 then
   echo "Installing OTF2 $OTF2_VERSION"
+  echo "Installing OTF2 $OTF2_VERSION" >> $BUILD_DIR/log.txt
   cd ~/
   wget http://www.vi-hps.org/upload/packages/otf2/otf2-$OTF2_VERSION.tar.gz
   tar -xzf otf2-$OTF2_VERSION.tar.gz
@@ -92,11 +105,14 @@ fi
 if [ ! -d $INSTALL_DIR/hpx ]
 then
   cd $INSTALL_DIR
-  git clone https://github.com/STEllAR-GROUP/hpx.git
+  git clone https://github.com/STEllAR-GROUP/hpx.git &>> $BUILD_DIR/log.txt
 fi
 cd $INSTALL_DIR/hpx
 # Set the state of the hpx repository to what it was on the requested date
-git checkout `git rev-list -1 --before="$BUILD_DATE" master`
+HPX_HASH=`git rev-list -1 --before="$BUILD_DATE" master`
+echo "Building HPX $HPX_HASH"
+echo "Building HPX $HPX_HASH" >> $BUILD_DIR/log.txt
+git checkout $HPX_HASH &>> $BUILD_DIR/log.txt
 
 # Build hpx
 DEFAULT_HPX_PARAMS="\
@@ -121,23 +137,26 @@ DEFAULT_HPX_PARAMS="\
  -DAPEX_WITH_PAPI=TRUE \
  -DHPX_WITH_MAX_CPU_COUNT=72"
 HPX_PARAMS=${HPX_PARAMS:-$DEFAULT_HPX_PARAMS}
-echo "HPX_PARAMS=\"$HPX_PARAMS\"" >> $BUILD_FILE
+echo "export HPX_PARAMS=\"$HPX_PARAMS\"" >> $BUILD_FILE
 rm -rf build
 mkdir build
 cd build
-cmake $HPX_PARAMS ..
-make -j $USE_PROCS -l $USE_PROCS
-make install
+cmake $HPX_PARAMS .. &>> $BUILD_DIR/log.txt
+make -j $USE_PROCS -l $USE_PROCS &>> $BUILD_DIR/log.txt
+make install &>> $BUILD_DIR/log.txt
 
 # Clone phylanx if needed
 if [ ! -d $INSTALL_DIR/phylanx ]
 then
   cd $INSTALL_DIR
-  git clone https://github.com/STEllAR-GROUP/phylanx.git
+  git clone https://github.com/STEllAR-GROUP/phylanx.git &>> $BUILD_DIR/log.txt
 fi
 cd $INSTALL_DIR/phylanx
 # Set the state of the phylanx repository to what it was on the requested date
-git checkout `git rev-list -1 --before="$BUILD_DATE" master`
+PHYLANX_HASH=`git rev-list -1 --before="$BUILD_DATE" master`
+echo "Building Phylanx $PHYLANX_HASH"
+echo "Building Phylanx $PHYLANX_HASH" >> $BUILD_DIR/log.txt
+git checkout $PHYLANX_HASH &>> $BUILD_DIR/log.txt
 
 # Build Phylanx
 DEFAULT_PHYLANX_PARAMS="\
@@ -149,10 +168,13 @@ DEFAULT_PHYLANX_PARAMS="\
   -Dblaze_DIR=$blaze_DIR \
   -Dpybind11_DIR=$pybind11_DIR"
 PHYLANX_PARAMS=${PHYLANX_PARAMS:-$DEFAULT_PHYLANX_PARAMS}
-echo "PHYLANX_PARAMS=\"$PHYLANX_PARAMS\"" >> $BUILD_FILE
+echo "export PHYLANX_PARAMS=\"$PHYLANX_PARAMS\"" >> $BUILD_FILE
 rm -rf build
 mkdir build
 cd build
-cmake $PHYLANX_PARAMS ..
-make -j $USE_PROCS -l $USE_PROCS
-make install
+cmake $PHYLANX_PARAMS .. &>> $BUILD_DIR/log.txt
+make -j $USE_PROCS -l $USE_PROCS &>> $BUILD_DIR/log.txt
+make install &>> $BUILD_DIR/log.txt
+
+echo "Finished retroactive build for $BUILD_DATE (unix timestamp for $HUMAN_BUILD_DATE)"
+echo "Finished retroactive build for $BUILD_DATE (unix timestamp for $HUMAN_BUILD_DATE)" >> $BUILD_DIR/log.txt
--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ for THREADS in 8 16 24 32 36
 do
   mkdir run$THREADS
   cd run$THREADS
-  srun -n 1 phylanx/bin/als_csv_instrumented \
+  srun -n 1 ../phylanx/bin/als_csv_instrumented \
     -t $THREADS \
     --data_csv=/phylanx-data/CSV/MovieLens_20m.csv \
     --hpx:bind=balanced \
@@ -21,6 +21,6 @@ do
     --iterations=1 \
     --f=40 \
     --row_stop=700 \
-    --col_stop=20000
+    --col_stop=20000 >stdout.txt 2>stderr.txt
   cd ..
 done
