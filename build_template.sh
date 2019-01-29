dcc3e4072f7c2d4ee794c043a2e00db8d8094eed
Alex Bigelow <alex.r.bigelow@gmail.com>
2019-01-29 12:21:07 -0700

    Fix ANSI string in build template

--- build_template.sh
+++ build_template.sh
@@ -7,7 +7,7 @@ USE_PROCS=$(($PROCS/2))
 LOG_FILE="$TARGET_DIR/log.txt"
 touch $LOG_FILE
 
-echo "\n\n\n#####\nStarting retroactive build for $BUILD_DATE (unix timestamp for $HUMAN_BUILD_DATE)" |& tee -a $LOG_FILE
+echo $'\n\n\n#####\nStarting retroactive build for $BUILD_DATE (unix timestamp for $HUMAN_BUILD_DATE)' |& tee -a $LOG_FILE
 
 # Install OTF2 if needed
 OTF2_DIR=${OTF2_DIR:-"$HOME/otf2"}
